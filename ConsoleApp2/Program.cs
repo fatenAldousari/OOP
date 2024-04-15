762d6437a21b8a22682c99ce0525f6f4e4e35511
KFH <KFH@LAPTOP-TRN-140>
2024-04-15 10:25:18 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+var student = new Student("faten","ce",4.0);
+student.StudentSummary();
+public class Student
+{
+
+    public string Name { get; set; }
+    public string Major { get; set; }
+    public double Gpa { get; set; }
+    public Student(string name, string major, double gpa)
+    {
+        Name = name;
+        Major = major;
+        Gpa = gpa;
+    }
+    public void StudentSummary()
+    {
+        Console.WriteLine($"$student {Name} major {Major} gpa {Gpa}");
+
+    }
+}
+
+
